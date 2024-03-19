5d8b16e18420e0e2ad74da38532fec9fb3d206c3
ahmed-mahmoud <158545155+ahmed-mahmoud005@users.noreply.github.com>
2024-03-19 09:14:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector(".img0");
+let container = document.querySelector(".container");
+function phones(x){
+    img.src = x ;
+}
+
+function colors(p){
+    container.style.backgroundColor = p ;
+}
